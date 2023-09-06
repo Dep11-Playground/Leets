8f30b80d2f8eb0a4cc051e4d7251ab766570fc8d
sasirijambugaswatta <86281435+sasirijambugaswatta@users.noreply.github.com>
2023-09-06 08:27:50 +0530

    Initial commit

---
+++ Palindrome.java
@@ -0,0 +1,15 @@
+class Palindrome{
+    public boolean isPalindrome(int x) {
+        int rev = 0;
+        int temp = 0;
+        String numstr = Integer.toString(x);
+        System.out.println(numstr);
+        while()
+        return true;
+    }
+    public static void main(String[] args) {
+        Palindrome p = new Palindrome();
+        p.isPalindrome(-121);
+
+    }
+}
